90fb28dd38f0fb9321ee35ad181f6e95ed2e6528
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-12-01 17:39:09 +0100

    sww

--- js/script.js
+++ js/script.js
@@ -1,46 +1,9 @@
-// 1
-let threeNumbers = [1, 2, 3]
-threeNumbers[1] = 10
-console.log(threeNumbers);
-// 2
-let drink = ["tea", "coffee", "juice"]
-drink[3] = "water"
-console.log(drink);
-// 3
-let numbers = [12, 7, 9, 65];
-let sum = 0;
-for (const num of numbers) {
-    sum += num;
-}
-console.log("Сума чисел:", sum);
-console.log("-----------------------------");
-// 4
-let fiveNumbers = [1, 2, 3, 4, 5];
-for (let i = 0; i < fiveNumbers.length; i++) {
-    console.log(fiveNumbers[i]);
-}
-console.log("------------------------------------");
-// 5
-let words = ["Cow", "Mountian", "Sunshine", "Elephant", "Tree"];
-for (let word of words) {
-    if (word.length >= 5) {
-        console.log(word);
-    }
-}
-// 6
-let tenNumbers = [7]
-let biggestNumber = tenNumbers[0]
-for (const number of tenNumbers) {
-    if (number > biggestNumber) {
-        biggestNumber = number
-    }
-}
-let tenNumbers2 = [3, 17, 42, 8, 29, 56, 91, 14, 63, 7];
-let evenNumbers = [];
-for (let i = 0; i < tenNumbers2.length; i++) {
-    if (tenNumbers2[i] % 2 === 0) {
-        evenNumbers.push(tenNumbers2[i]);
-    }
-  
-}
-console.log(evenNumbers);
+import Handlebars from "../node_modules/handlebars";
+import template from "./templates/products.hbs";
+import { products } from "../data.js";
+
+const container = document.getElementById("products-container");
+
+const html = template({ products });
+
+container.innerHTML = html;
